03879453976f8910ba1619d0f625d87da60f7a73
kartik <kavimandankartik33@gmail.com>
2021-04-01 01:34:50 +0700

    Added: Add Name

--- Reg.java
+++ Reg.java
@@ -1,1 +1,28 @@
-#Welcome to Regex Problems
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Reg {
+	
+	static String fName;
+
+		static String firstName(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your first name : ");
+		fName = sc.nextLine();
+		return fName;
+	}
+
+	public static void main(String args[]) {
+	
+		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",Reg.firstName());
+
+		if(Boolean.TRUE.equals(firstName))
+		{
+			System.out.println("Correct format");
+		}
+		else
+		{
+			System.out.println("Incorrect format try   K artik this format");
+		}
+}
+}
