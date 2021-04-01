bb17d4e525d0438a90d2ce626bbec6d44bae35ef
kartik <kavimandankartik33@gmail.com>
2021-04-01 11:35:49 +0700

    Added: First name and Last name

--- Reg.java
+++ Reg.java
@@ -2,27 +2,49 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class Reg {
-	
+
 	static String fName;
+	static String lName;
 
-		static String firstName(){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your first name : ");
-		fName = sc.nextLine();
-		return fName;
-	}
+   
+   static String firstName(){
+      Scanner sc1 = new Scanner(System.in);
+      System.out.println("Enter your first name first letter must be Capital");
+      fName = sc1.nextLine();
+      return fName;
+   }
 
-	public static void main(String args[]) {
 	
-		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",Reg.firstName());
+	static String lastName(){
+      Scanner sc2 = new Scanner(System.in);
+      System.out.println("Enter your last name first letter must me Capital");
+      lName = sc2.nextLine();
+      return lName;
+   }
+	
 
-		if(Boolean.TRUE.equals(firstName))
-		{
-			System.out.println("Correct format");
-		}
-		else
-		{
-			System.out.println("Incorrect format try   K artik this format");
-		}
-}
+   public static void main(String args[]) {
+
+
+      
+      Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",Reg.firstName());
+
+      Boolean lastName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",Reg.lastName());
+
+      
+      if (firstName == true) {
+          System.out.println("First Name: " + fName);
+      
+      }
+	else {
+		System.out.println("Invalid");}
+
+		if (lastName == true) {
+           System.out.println("Last Name: " + lName);
+      }
+	else {
+	System.out.println("invalid");}
+
+		
+	}
 }
