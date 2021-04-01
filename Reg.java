80b6c03355cedaf2250219d78403217b3953251d
kartik <kavimandankartik33@gmail.com>
2021-04-01 20:17:50 +0700

    Added: Pasword 3

--- Reg.java
+++ Reg.java
@@ -53,7 +53,7 @@ static String firstName(){
 
 	      Boolean emailId = Pattern.matches("^[\\w-\\+]+(\\.[\\w]+)@[\\w-]+(\\.[\\w]+)(\\.[a-z]{2,})$",Reg.emailId());
 		
-	       Boolean Pasword = Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}",Reg.Pasword());
+	       Boolean Pasword = Pattern.matches("^(?=.*[@#$%^&-+=()])(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}",Reg.Pasword());
 
 
 		    
